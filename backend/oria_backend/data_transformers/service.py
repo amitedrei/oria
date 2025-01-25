0d2649ab6e502b465f4c0a115e4b9d0632b82d98
Jo0L <akark2345@gmail.com>
2025-01-25 15:10:30 +0200

    add text to emotion route

--- backend/oria_backend/data_transformers/service.py
+++ backend/oria_backend/data_transformers/service.py
@@ -1,17 +1,18 @@
 import numpy as np
 from scipy.spatial.distance import cosine, euclidean
-from transformers import AutoModel
+from transformers import AutoModel, pipeline
 
 from .models import (
     DistanceResponseModel,
     EmbeddingsResponseModel,
     TextToEmbeddingsModel,
+    EmotionsResponseModel, 
+    TextToEmotionsModel
 )
 
 embeddings_model = AutoModel.from_pretrained(
     "jinaai/jina-embeddings-v3", trust_remote_code=True
 )
-
 
 async def get_embeddings(data: TextToEmbeddingsModel) -> EmbeddingsResponseModel:
     embeddings = embeddings_model.encode([data.text])
@@ -27,3 +28,14 @@ def calculate_distance(
     cosine_sim = 1 - cosine(embedding1, embedding2)
 
     return DistanceResponseModel(similarity=cosine_sim)
+
+async def get_text_emotion(data: TextToEmotionsModel) -> EmotionsResponseModel:
+    classifier_emotion = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
+    emotions = classifier_emotion(data.text)[0]
+
+    classifier_label = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    LABELS = ["Chill", "Energetic", "Intense", "Uplifting", "Dark", "Melancholic",  "Playful", "Romantic", "Workout", "Relaxation", "Study", "Driving", "Party", "Focus", "Meditation", "Nature"]
+    labels_scores = classifier_label(data.text, LABELS, multi_label=True)
+
+    emotions.extend([{'label': label, 'score': score} for label, score in zip(labels_scores['labels'], labels_scores['scores'])])
+    return EmotionsResponseModel(text=data.text, emotions=emotions)
