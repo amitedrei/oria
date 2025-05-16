b7ed353747a7b113a4ea2e3a31098a489e457a88
Amit <edreiamit@gmail.com>
2025-05-16 12:50:38 +0300

    bugfix

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd frontend && npm run build
+cd frontend && npm install && npm run build
 mkdir ../backend/oria_backend/static && cp -r build/* ../backend/oria_backend/static/
 cd ../backend && docker build --platform linux/amd64  -t test-backend .
