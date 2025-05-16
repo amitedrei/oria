3c15cdb4de514265053390c82704314c9d110d35
Amit <edreiamit@gmail.com>
2025-05-16 12:56:20 +0300

    Refactor build script to separate directory creation and file copying

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd frontend && npm install && npm run build
-mkdir ../backend/oria_backend/static && cp -r build/* ../backend/oria_backend/static/
+mkdir ../backend/oria_backend/static
+cp -r build/* ../backend/oria_backend/static/
 cd ../backend && docker build --platform linux/amd64  -t test-backend .
